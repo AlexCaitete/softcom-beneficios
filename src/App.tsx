ebdda9b4b05f7ca7c53d012b4e06eff741ab5114
jessicabatista <jessicabatista.dev@gmail.com>
2026-05-21 18:20:15 -0300

    minhas alteraçoes 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import "./tailwind.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/softcom-home/Softcom';
+
 import { Toaster } from "sonner";
 
 import { Layout } from "./components/adm-page/Layout";
+<<<<<<< HEAD
 // Adicione esta linha:
 import { AdminPage } from "./components/adm-page/AdminPage";
 import { EmployeePage } from "./pages/EmployeePage";
+=======
+
+import { AdminPage } from "./components/adm-page/AdminPage";
+import { EmployeePage } from "./components/funcionario-page/EmployeePage";
+>>>>>>> origin/Branch-Myla
 
 function App() {
   return (
     <BrowserRouter>
       <Toaster position="top-right" richColors />
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/funcionario-page" element={<EmployeePage />} />
+        
         <Route path="/" element={<Layout />}>
           <Route path="admin" element={<AdminPage />} />
           <Route path="meu-perfil" element={<EmployeePage />} />
